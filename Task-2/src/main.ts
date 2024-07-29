26da5a5b4ae39bf157ec4dfabda91e412c5ccdb9
Никита <kaktusloco@gmail.com>
2024-07-29 09:49:59 +0300

    update Task-2

--- Task-2/src/main.ts
+++ Task-2/src/main.ts
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "./assets/bootstrap.min.css";
 import "./assets/main.css";
 
 import { createApp } from "vue";
